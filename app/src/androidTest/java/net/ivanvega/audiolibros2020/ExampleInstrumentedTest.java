403ef8562f86fd8daf4c1df3cea1313a705ef756
EdgarBryanLM <55665769+EdgarBryanLM@users.noreply.github.com>
2020-11-09 16:34:24 -0600

    variable

---
+++ app/src/androidTest/java/net/ivanvega/audiolibros2020/ExampleInstrumentedTest.java
@@ -0,0 +1,28 @@
+package net.ivanvega.audiolibros2020;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        int numero;
+        // Context of the app under test.
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("net.ivanvega.audiolibros2020", appContext.getPackageName());
+    }
+}
